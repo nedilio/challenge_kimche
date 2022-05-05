2302aab5d6cdafe39353947b448bec5f8336a02b
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-05-05 12:29:01 -0400

    Updates

---
+++ precache-manifest.efafbcf8f8781d291b66ce4fd09c61be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edef66729a0452c30502c9929bb1c3ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c881b200983c7ced1221",
+    "url": "/static/css/main.dc181e90.chunk.css"
+  },
+  {
+    "revision": "3f7e645ecea306abf864",
+    "url": "/static/js/2.d8a3312b.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.d8a3312b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c881b200983c7ced1221",
+    "url": "/static/js/main.4bd2b1fb.chunk.js"
+  },
+  {
+    "revision": "f79177c24e5f8a16816b",
+    "url": "/static/js/runtime-main.6e9a7329.js"
+  }
+]);
