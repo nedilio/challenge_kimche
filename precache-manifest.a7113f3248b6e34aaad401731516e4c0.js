d58310c8ce4fabdd92e0241d0c463fc80929c34f
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-05-06 10:53:35 -0400

    Updates

---
+++ precache-manifest.a7113f3248b6e34aaad401731516e4c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efafc096d5e447035145d29cd0f5ce6",
+    "url": "/challenge_kimche/index.html"
+  },
+  {
+    "revision": "f34400daea3e2732f4d8",
+    "url": "/challenge_kimche/static/css/main.c7144104.chunk.css"
+  },
+  {
+    "revision": "709f3b0998e187903e5d",
+    "url": "/challenge_kimche/static/js/2.aa3f6145.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/challenge_kimche/static/js/2.aa3f6145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34400daea3e2732f4d8",
+    "url": "/challenge_kimche/static/js/main.584f14fc.chunk.js"
+  },
+  {
+    "revision": "af2051bf7d854e6a5755",
+    "url": "/challenge_kimche/static/js/runtime-main.342828c2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/challenge_kimche/precache-manifest.1fce527e0844d6b3c343105f6fd9d376.js"
+  "/challenge_kimche/precache-manifest.a7113f3248b6e34aaad401731516e4c0.js"
 );
 
 self.addEventListener('message', (event) => {
