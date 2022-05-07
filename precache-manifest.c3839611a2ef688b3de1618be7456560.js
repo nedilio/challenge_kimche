bb6621722af6df5599eeeec7930993cb03093ec6
Nelson Izquierdo <izquierdonelson@gmail.com>
2022-05-06 20:06:41 -0400

    Updates

--- precache-manifest.c3839611a2ef688b3de1618be7456560.js
+++ precache-manifest.c3839611a2ef688b3de1618be7456560.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fbbb278a72d104b377c154945f796a7",
+    "revision": "70126bc2283bc8ff6c3f4eab18eebe88",
     "url": "/challenge_kimche/index.html"
   },
   {
-    "revision": "f56ac62975f77a66ee5d",
+    "revision": "8fc832c524af4a07d4f6",
     "url": "/challenge_kimche/static/css/main.0aeb6eef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/challenge_kimche/static/js/2.aa3f6145.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f56ac62975f77a66ee5d",
-    "url": "/challenge_kimche/static/js/main.0a3607d8.chunk.js"
+    "revision": "8fc832c524af4a07d4f6",
+    "url": "/challenge_kimche/static/js/main.538b5a5b.chunk.js"
   },
   {
     "revision": "af2051bf7d854e6a5755",
